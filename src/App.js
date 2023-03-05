b093f3f45c82e2cf96ea85a0b6e452a624846fc2
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-05 11:33:28 -0300

    :sparkles: feat: switching between routes with JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
+
+console.log(window.location)
+
+const pagina = window.location.pathname === '/' ? <Home /> : <AboutMe />
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    pagina
   );
 }
 
