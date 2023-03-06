ce601cfa86ecd2a241fcf3f1e015c509ef20cab5
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-06 05:27:11 -0300

    :recycle: refactor: changing useLocation to NavLink component

--- src/components/LinkMenu/index.js
+++ src/components/LinkMenu/index.js
@@ -1,17 +1,18 @@
-import { Link, useLocation } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './LinkMenu.module.scss'
 
 const LinkMenu = ({children, to}) => {
-
-    const navigation = useLocation()
     
     return (
-        <Link 
-            className={`${styles.link} ${navigation.pathname === to ? styles.link__underline : ''}`} 
+        <NavLink 
+            className={({ isActive }) => `
+                ${styles.link}
+                ${isActive ? styles.link__underline : ""}
+            `} 
             to={to}
         >
             {children}
-        </Link>
+        </NavLink>
     )
 }
 
