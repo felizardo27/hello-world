8dd0049a32b09739207c0b2ab55aab4174321d75
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-03-07 05:49:59 -0300

    :sparkles: feat: dinamizing Post data

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,14 +1,20 @@
 import { useParams } from 'react-router-dom'
 import styles from './Post.module.scss'
+import posts from 'json/posts.json'
+import BasePost from 'components/BasePost'
 
 const Post = () => {
-
     const params = useParams()
 
+    const post = posts.find(post => post.id === Number(params.id))
+
     return (
-        <h1>
-            post {params.id}
-        </h1>
+        <BasePost
+            photoCover={`/assets/posts/${post.id}/capa.png`}
+            title={post.title}
+        >
+            texto...
+        </BasePost>
     )
 }
 
